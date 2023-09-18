601a8ef0d45f5a4c3d154d44754b555a909e53b0
AlejandroSierraE <sierraescobaralejandro@gmail.com>
2023-09-18 11:08:43 -0500

    feat: :recycle: Se añade el formulario de contacto con sus validaciones
    Se añade el formulario de contacto con sus validaciones

--- js/index.js
+++ js/index.js
@@ -172,3 +172,136 @@ ourServices.map((service) => {
     servicesContainer.innerHTML += serviceHTML
 })
 
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+/*validaciones form contactanos*/ 
+// Obtén el formulario y los campos de entrada
+const form = document.getElementById('myContactForm');
+const nameInput = document.getElementById('name');
+const lastNameInput = document.getElementById('lastName');
+const emailInput = document.getElementById('email');
+const cellPhoneInput = document.getElementById('cellPhone');
+const affairInput = document.getElementById('affair');
+const messageInput = document.getElementById('messaje');
+
+//Limpiar formulario
+const clearForm = () =>{
+    nameInput.value = "";
+    lastNameInput.value = "";
+    emailInput.value = "";
+    cellPhoneInput.value = "";
+    affairInput.value = "";
+    messageInput.value = "";
+
+    nameInput.classList.remove('is-valid');
+    lastNameInput.classList.remove('is-valid');
+    emailInput.classList.remove('is-valid');
+    cellPhoneInput.classList.remove('is-valid');
+    affairInput.classList.remove('is-valid');
+    messageInput.classList.remove('is-valid');
+}
+
+// Agrega un evento de escucha al formulario
+enviar.addEventListener('click', function (event) {
+  // Variable para rastrear si hay errores de validación
+  let hasError = false;
+
+  // Validación del campo de nombres
+  const onlyLetters = /^[A-Za-zÁáÉéÍíÓóÚúÜüÑñ]+$/;
+  if (nameInput.value.trim() === '' || !onlyLetters.test(nameInput.value)) {
+    nameInput.classList.add('is-invalid');
+    hasError = true;
+  }else{
+    nameInput.classList.remove('is-invalid');
+    nameInput.classList.add('is-valid');
+  }
+
+  // Validación del campo de apellidos
+  if (lastNameInput.value.trim() === '') {
+    lastNameInput.classList.add('is-invalid');
+    hasError = true;
+  }
+  else{
+    lastNameInput.classList.remove('is-invalid');
+    lastNameInput.classList.add('is-valid');
+  }
+
+  // Validación del campo de correo electrónico
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  if (!emailPattern.test(emailInput.value)) {
+    emailInput.classList.add('is-invalid');
+    hasError = true;
+  }
+  else{
+    emailInput.classList.remove('is-invalid');
+    emailInput.classList.add('is-valid');
+  }
+
+  // Validación del campo de número de celular
+  const onlyNumbers = /^[0-9]+$/;
+  if ((!onlyNumbers.test(cellPhoneInput.value)) || (cellPhoneInput.value.length < 10)) {
+    cellPhoneInput.classList.add('is-invalid');
+    hasError = true;
+  }else{
+    cellPhoneInput.classList.remove('is-invalid');
+    cellPhoneInput.classList.add('is-valid');
+  }
+
+  // Validación del campo de asunto
+  if (affairInput.value.trim() === '') {
+    affairInput.classList.add('is-invalid');
+    hasError = true;
+  }else{
+    affairInput.classList.remove('is-invalid');
+    affairInput.classList.add('is-valid');
+  }
+
+  // Validación del campo de mensaje
+  if (messageInput.value.trim() === '') {
+    messageInput.classList.add('is-invalid');
+    hasError = true;
+  }else{
+    messageInput.classList.remove('is-invalid');
+    messageInput.classList.add('is-valid');
+  }
+
+  // Si hay errores, evita que el formulario se envíe
+  if (hasError) {
+    event.preventDefault();
+  }else{
+    const dataContact = {
+        nombres: nameInput.value,
+        apellidos: lastNameInput.value,
+        email: emailInput.value,
+        numCelular: cellPhoneInput.value,
+        asunto: affairInput.value,
+        mensaje: messageInput.value,
+    }
+
+    // Recupera los datos existentes del LocalStorage (si los hay)
+    const existingData = localStorage.getItem("formData");
+
+    // Convierte los datos existentes en un objeto JavaScript o crea uno nuevo si no hay datos existentes
+    const existingDataObj = existingData ? JSON.parse(existingData) : {};
+
+    // Combina los nuevos datos (objeto 'dataContact') con los datos existentes
+    const newDataKey = Date.now(); // Usamos una marca de tiempo como clave única para cada conjunto de datos
+    existingDataObj[newDataKey] = dataContact;
+
+    // Convierte el objeto combinado en una cadena JSON
+    const jsonData = JSON.stringify(existingDataObj);
+
+    // Almacena la cadena JSON en el LocalStorage
+    localStorage.setItem("formData", jsonData);
+
+    // Recupera el objeto JSON del LocalStorage más tarde si es necesario
+    const objetoRecuperado = JSON.parse(localStorage.getItem("formData"));
+
+    // Ahora objetoRecuperado contiene todos los conjuntos de datos almacenados previamente
+    //console.log(objetoRecuperado);
+
+    swal("Gracias por contactarnos!", "El formulario se ha enviado correctamente y pronto nos comunicaremos contigo!", "success");
+    clearForm();
+  }
+});
+
